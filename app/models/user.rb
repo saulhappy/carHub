e9c874cd117e073940d717e9f1f6a11de28fc12a
Vincent Marks <violateacher123@gmail.com>
2019-10-24 16:28:58 -0500

    time to work on maint records

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
     has_secure_password
 
     has_one :family
+    has_many :user_cars
     has_many :cars, through: :user_cars
-    
+
     validates :name, presence: true
     validates :name, uniqueness: true
 end
